669f35a07f668c2174d37dafdd64ba5c354ff688
madhursharma026 <105696243+madhursharma026@users.noreply.github.com>
2024-04-10 23:12:00 +0530

    Initial Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { ThemeProvider } from './components/ThemeContext.js.js';
+import { ThemeProvider } from './components/ThemeContext.js';
 
 export default function App({ Component, pageProps }) {
   return (
